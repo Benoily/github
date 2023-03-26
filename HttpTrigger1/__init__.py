7f0f3a8f0c01bd5132e2f91ceb3891b868db0bc2
Benoily <fbenoily@yahoo.com>
2023-03-26 12:53:20 +0200

    firT commit

---
+++ HttpTrigger1/__init__.py
@@ -0,0 +1,73 @@
+import logging
+import os, uuid
+import pandas as pd
+import pickle
+import urllib
+#import surprise
+from math import *
+from heapq import nlargest
+import function_extern as fc
+#import numpy as np
+
+
+import azure.functions as func
+logging.info('Python HTTP trigger function processed a request.')
+
+url = 'https://stoch.blob.core.windows.net/stock/articles.pickle'
+f = urllib.request.urlopen(url)
+article = pickle.load(f)
+
+url = 'https://stoch.blob.core.windows.net/stock/click.pickle'
+f = urllib.request.urlopen(url)
+click = pickle.load(f)
+
+url = 'https://stoch.blob.core.windows.net/stock/model_SVD.pickle'
+f = urllib.request.urlopen(url)
+model = pickle.load(f)
+
+
+
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        id = req_body.get('id')
+        type = req_body.get('type')
+
+    if isinstance(id, int) and isinstance(type, str):
+        if type == "cb":
+            pred_recom = fc.contentBasedRecommendArticle(article, click, id)
+        elif type == "cf":
+            pred_recom = fc.model_recommand(model , article, click, id)
+        else:
+            pred_recom = 'Choice the type.'
+
+        #pred_recom = fc.model_recommand(model , article, click, id) if type == "cb" else fc.contentBasedRecommendArticle(article, click, id)
+
+
+    #name = req.params.get('name')
+    #if not name:
+        #try:
+            #req_body = req.get_json()
+        #except ValueError:
+            #pass
+        #else:
+            #name = req_body.get('name')
+
+    #if name:
+        #pred = fc.model_recommand(model , article, click, name)
+        #pred2 = fc.contentBasedRecommendArticle(article, click, name)
+      
+        return func.HttpResponse(str(pred_recom),status_code=200)
+    else:
+        return func.HttpResponse(
+             "Requete invalide .",
+             status_code=400
+        )
---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import requests
+
+
+# Ajout d'un titre 
+st.title('My Content - Recommandation d\'articles')
+
+# Ajout d'un logo
+st.image('logo.png', width=200)
+
+# Ajout d'un texte de présentation 
+st.markdown("Bienvenue sur My Content ! Veuillez renseigner ci-dessous pour voir les articles que nous vous recommandons.")
+
+
+#if api_url == '':
+#api_url = 'https://recomt.azurewebsites.net/api/HttpTrigger1?'
+
+# Récupération de l'id 
+type = st.text_input('Entrez votre type (Mettez **cb** ou **cf**) : ')
+
+# Récupération de l'id 
+id = st.text_input('Entrez votre id : ')
+
+if st.button("Soumettre"):
+    with st.spinner("Recommandation d'articles en cours..."):
+        if id:
+            params = {"id": int(id), "type": type }
+    
+            r = requests.get('https://recmt.azurewebsites.net/api/HttpTrigger1?', json=params, verify=True)
+            content = r.content.decode("utf-8")
+    
+            remove ='[ ]'
+            for charac in remove:
+                content = content.replace(charac, '')
+
+            content = content.split(',')
+
+            try:
+                response = content
+    #try:
+    #    response = requests.get('https://recomt.azurewebsites.net/api/HttpTrigger1?', json=params)
+            except Exception as e:
+                 st.error(e)
+            st.info("Les résultats sont classés par ordre de pertinence.")
+            st.write('Voici les articles recommandés pour l\'utilisateur ' + str(id) + ' :')
+            st.write(response)
+st.markdown("made by **FOLY BENOIT KUEVIAKOE**")
